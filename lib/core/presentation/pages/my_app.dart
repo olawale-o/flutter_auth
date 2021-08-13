8039f69a4a2504201fe7db753291b857c0d68fed
olawale-o <omoogunolawale@gmail.com>
2021-08-13 11:58:13 +0100

    Add core/presentation directory

---
+++ lib/core/presentation/pages/my_app.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../../routes/routes.dart';
+import '../../routes/route.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Auth',
+      initialRoute: initial_route,
+      onGenerateRoute: GeneratedRoute.onGenerateRoute,
+    );
+  }
+}
