a6a875da72414d00adbd72be3fb9ade87ad2423e
olawale-o <omoogunolawale@gmail.com>
2021-08-13 18:13:56 +0100

    Register NavigationBloc in injection_container.dart file

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
+import 'core/navigation/navigation_bloc.dart';
+
 final s1 = GetIt.instance;
 
 Future<void> init() async {
-
+  // blocs
+  s1.registerFactory(() => NavigationBloc(s1()));
+  
   final GlobalKey<NavigatorState> navigatorKey =  GlobalKey<NavigatorState>();
   s1.registerLazySingleton(() => navigatorKey);
 
