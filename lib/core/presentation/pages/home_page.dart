e105c92da63c27f273627b4f237c49691a244cd5
olawale-o <omoogunolawale@gmail.com>
2021-08-13 16:45:30 +0100

    Add home_page.dart file

---
+++ lib/core/presentation/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/core/presentation/pages/splash_page.dart';
+import 'package:flutter_auth/core/routes/routes.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final Future<FirebaseApp> _initialize = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+          future: _initialize,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Error ${snapshot.error}'),);
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return SplashPage();
+            }
+            return Center(child: CircularProgressIndicator(),);
+          }
+      );
+  }
+}
