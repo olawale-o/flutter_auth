faa1e103d7326dabd10e624df9f0e769b90b48ef
olawale-o <omoogunolawale@gmail.com>
2021-08-13 18:12:47 +0100

    Update main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'core/presentation/pages/my_app.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(MyApp());
 }
