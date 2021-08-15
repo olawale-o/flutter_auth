7965d4dc99f520ae8bf73a295ec6374fadf171fb
olawale-o <omoogunolawale@gmail.com>
2021-08-15 16:15:05 +0100

    Add dashboard_page.dart file

---
+++ lib/core/presentation/pages/dashboard_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({Key? key}) : super(key: key);
+
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard page'),
+      ),
+      body: Center(
+        child: Text('Logged in as'),
+      ),
+    );
+  }
+}
