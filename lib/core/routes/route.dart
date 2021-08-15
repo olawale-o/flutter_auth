78b90ad32bb13e0b0eb6c793ecfdb2e330b23672
olawale-o <omoogunolawale@gmail.com>
2021-08-15 16:19:27 +0100

    Import DashBoardPage in route.dart file

--- lib/core/routes/route.dart
+++ lib/core/routes/route.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/core/presentation/pages/home_page.dart';
-import 'package:flutter_auth/core/presentation/pages/splash_page.dart';
-import 'package:flutter_auth/core/routes/routes.dart';
-import 'package:flutter_auth/features/email_auth/presentation/pages/login_page.dart';
+import 'routes.dart';
+import '../presentation/pages/dashboard_page.dart';
+import '../presentation/pages/home_page.dart';
+import '../presentation/pages/splash_page.dart';
+import '../../../features/email_auth/presentation/pages/login_page.dart';
 
 class GeneratedRoute {
   static Route<dynamic> onGenerateRoute(RouteSettings routeSettings) {
     final args = routeSettings.arguments;
     switch(routeSettings.name) {
+      case dashboard_page:
+        return MaterialPageRoute(builder: (_) => DashboardPage());
       case home_page:
         return MaterialPageRoute(builder: (_) => Homepage());
       case splash_page:
