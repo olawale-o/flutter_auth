8fd34b826271d398038c06e4b83982334d3dc328
olawale-o <omoogunolawale@gmail.com>
2021-08-15 17:10:28 +0100

    Add new field to AuthButton constructor

--- lib/features/email_auth/presentation/widgets/auth_button.dart
+++ lib/features/email_auth/presentation/widgets/auth_button.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/core/navigation/navigation_bloc.dart';
+import 'package:flutter_auth/core/routes/routes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AuthButton extends StatelessWidget {
   final String logo;
   final String text;
-  const AuthButton({Key? key, required this.logo, required this. text}) : super(key: key);
+  final String page;
+  const AuthButton({Key? key,
+    required this.logo, required this.text, required this.page
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +17,8 @@ class AuthButton extends StatelessWidget {
       margin: const EdgeInsets.symmetric(vertical: 7.0),
       child: OutlinedButton(
           onPressed: () => {
-            print("pressed")
+          BlocProvider.of<NavigationBloc>(context)
+              .add(NavigationPushName(route: page))
           },
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 12.0),
