a011c51f44b2c11d39e702f80b042536e0f5e6dd
olawale-o <omoogunolawale@gmail.com>
2021-08-22 15:24:45 +0100

    Add signup_page route

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -3,4 +3,5 @@ const home_page = 'home_page';
 const splash_page = 'splash_page';
 const dashboard_page = 'dashboard_page';
 const login_form = 'login_form';
+const signup_page = 'signup_page';
 const initial_route = home_page;
