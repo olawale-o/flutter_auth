97e0340bf14a9ce67488a46557cd589430c2cfe6
olawale-o <omoogunolawale@gmail.com>
2021-08-22 15:32:56 +0100

    Import signup_page.dart in route.dart file

--- lib/core/routes/route.dart
+++ lib/core/routes/route.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/features/email_auth/presentation/forms/login_form.dart';
+import '../../features/email_auth/presentation/forms/login_form.dart';
+import '../../features/email_auth/presentation/pages/signup_page.dart';
 import 'routes.dart';
 import '../presentation/pages/dashboard_page.dart';
 import '../presentation/pages/home_page.dart';
@@ -21,6 +22,8 @@ class GeneratedRoute {
         return MaterialPageRoute(builder: (_) => SplashPage());
       case login_page:
         return MaterialPageRoute(builder: (_) => LoginPage());
+      case signup_page:
+        return MaterialPageRoute(builder: (_) => SignupPage());
       default:
         return _errorRoute();
     }
