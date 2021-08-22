529c6385cd7240a3931a2bcbc9ceb263ccbb9571
olawale-o <omoogunolawale@gmail.com>
2021-08-22 15:42:14 +0100

    Modify login_page.dart file

--- lib/features/email_auth/presentation/pages/login_page.dart
+++ lib/features/email_auth/presentation/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/core/navigation/navigation_bloc.dart';
 import 'package:flutter_auth/core/routes/routes.dart';
 import 'package:flutter_auth/features/email_auth/presentation/widgets/auth_button.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -21,10 +23,26 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              AuthButton(logo: "assets/user.png", text: "User email or phone", page: login_form,),
-              AuthButton(logo: 'assets/google.png', text: "Continue with google" , page: login_form,),
-              AuthButton(logo: "assets/twitter.png", text: "Continue with twitter" , page: login_form,),
-              AuthButton(logo: "assets/facebook.png", text: "Continue with facebook" , page: login_form,),
+              Expanded(child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AuthButton(logo: "assets/user.png", text: "User email or phone", page: login_form,),
+                  AuthButton(logo: 'assets/google.png', text: "Continue with google" , page: login_form,),
+                  AuthButton(logo: "assets/twitter.png", text: "Continue with twitter" , page: login_form,),
+                  AuthButton(logo: "assets/facebook.png", text: "Continue with facebook" , page: login_form,),
+                ],
+              )),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have an account? '),
+                    TextButton( onPressed: () => BlocProvider.of<NavigationBloc>(context).add(NavigationPushName(route: signup_page)),
+                      child: Text('Sign up'),)
+                  ],
+                ),
+              )
             ],
           ),
         )
