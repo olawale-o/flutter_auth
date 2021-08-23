68c5565659fb4610d2273889df19e1f12e539dd7
olawale-o <omoogunolawale@gmail.com>
2021-08-23 22:32:54 +0100

    Register AuthRemoteDataSource in injection_container.dart file

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'features/email_auth/data/datasource/auth_remote_datasource.dart';
 import 'package:get_it/get_it.dart';
 
 import 'core/navigation/navigation_bloc.dart';
@@ -9,8 +11,12 @@ Future<void> init() async {
   // blocs
   s1.registerFactory(() => NavigationBloc(s1()));
 
+  s1.registerLazySingleton<AuthRemoteDataSource>(() => AuthRemoteDataSourceImpl(firebaseAuth: s1()));
+
   final GlobalKey<NavigatorState> navigatorKey =  GlobalKey<NavigatorState>();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   s1.registerLazySingleton(() => navigatorKey);
+  s1.registerLazySingleton(() => firebaseAuth);
 
   print('initialized');
 }
