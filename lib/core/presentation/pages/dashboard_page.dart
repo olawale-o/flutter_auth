8c00ceec5ee2e937f0b53ccc6fe213b9fd0ab769
olawale-o <omoogunolawale@gmail.com>
2021-08-23 21:22:35 +0100

    Modify dashboard_page.dart file

--- lib/core/presentation/pages/dashboard_page.dart
+++ lib/core/presentation/pages/dashboard_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/core/helper/auth_helper.dart';
+import 'package:flutter_auth/core/navigation/navigation_bloc.dart';
+import 'package:flutter_auth/core/routes/routes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -8,11 +12,19 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  AuthHelper _authHelper = AuthHelper();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dashboard page'),
+        actions: [
+          IconButton(onPressed: () async {
+            await _authHelper.logOut();
+            BlocProvider.of<NavigationBloc>(context)
+                .add(NavigationPushName(route: login_page));
+          }, icon: Icon(Icons.home))
+        ],
       ),
       body: Center(
         child: Text('Logged in as'),
