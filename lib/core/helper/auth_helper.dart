3a17249567471a47deb29c7a15844b086c81aa15
olawale-o <omoogunolawale@gmail.com>
2021-08-23 21:20:09 +0100

    Create logOut function in auth_helper.dart file

--- lib/core/helper/auth_helper.dart
+++ lib/core/helper/auth_helper.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_auth/features/email_auth/domain/entity/user_entity.dart';
 
 class AuthHelper {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
@@ -8,6 +9,12 @@ class AuthHelper {
     try{
       UserCredential _userCredential = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
+      UserEntity userEntity = UserEntity(
+          additionalUserInfo: _userCredential.additionalUserInfo,
+          user: _userCredential.user,
+          credential: _userCredential.credential
+      );
+      print('userEntity: $userEntity');
       print('User $_userCredential');
     } on FirebaseAuthException catch(e){
       if (e.code == 'weak-password') {
@@ -24,6 +31,12 @@ class AuthHelper {
     try{
       UserCredential _userCredential = await _firebaseAuth
           .signInWithEmailAndPassword(email: email, password: password);
+      UserEntity userEntity = UserEntity(
+          additionalUserInfo: _userCredential.additionalUserInfo,
+          user: _userCredential.user,
+          credential: _userCredential.credential
+      );
+      print('userEntity: $userEntity');
       print('User $_userCredential');
     } on FirebaseAuthException catch(e) {
         if (e.code == 'user-not-found') {
@@ -34,5 +47,9 @@ class AuthHelper {
     } catch(e) {
       print('error $e');
     }
+  }
+
+  Future logOut() async {
+    await _firebaseAuth.signOut();
   }
 }
