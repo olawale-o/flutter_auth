de94ddf56270004d22ebb909a61f33ae16845164
olawale-o <omoogunolawale@gmail.com>
2021-08-24 16:07:47 +0100

    Fix files import in auth_bloc.dart

--- lib/features/email_auth/presentation/bloc/auth_bloc.dart
+++ lib/features/email_auth/presentation/bloc/auth_bloc.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
-
-import 'package:bloc/bloc.dart';
-import 'package:flutter_auth/core/domain/usecase.dart';
-import 'package:flutter_auth/features/email_auth/data/model/user_model.dart';
-import 'package:flutter_auth/features/email_auth/domain/usecases/auth_login_usecase.dart';
-import 'package:flutter_auth/features/email_auth/domain/usecases/auth_sigup_usecase.dart';
 import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import '../../../../core/domain/usecase.dart';
+import '../../../../core/exceptions/failure.dart';
+import '../../data/model/user_model.dart';
+import '../../domain/usecases/auth_login_usecase.dart';
+import '../../domain/usecases/auth_sigup_usecase.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
@@ -27,12 +28,24 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   ) async* {
     if (event is AuthSignUpEvent) {
       yield(AuthLoading());
-      final user = await authSignUpUseCase(Params(email: event.email, password: event.password));
-      yield user.fold((e) => AuthError(message: e.toString()), (u) => AuthLoaded(userModel: u));
+      final failureOrUser = await authSignUpUseCase(Params(email: event.email, password: event.password));
+      yield* _eitherFailureOrLoaded(failureOrUser);
     } else if (event is AuthLoginEvent) {
       yield AuthLoading();
-      final user = await authLoginUseCase(Params(email: event.email, password: event.password));
-      yield user.fold((e) => AuthError(message: e.toString()), (u) => AuthLoaded(userModel: u));
+      final failureOrUser = await authLoginUseCase(Params(email: event.email, password: event.password));
+      yield* _eitherFailureOrLoaded(failureOrUser);
     }
+  }
+
+  Stream<AuthState> _eitherFailureOrLoaded(Either<Failure, UserModel> failureOrUser) async* {
+    yield failureOrUser.fold((e) => AuthError(message: _buildError(e)), (u) => AuthLoaded(userModel: _buildUser(u)));
+  }
+
+  UserModel _buildUser(UserModel authState) => authState;
+
+  String _buildError(Failure failure) {
+    if (failure is NetworkFailure) return 'Check your internet settings';
+    if (failure is ServerFailure) return 'Unable to connect server';
+    return 'Uncaught error';
   }
 }
