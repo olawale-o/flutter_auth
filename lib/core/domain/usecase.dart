a5aef4a86399a8b5bdd263bc12a8994c0b9b5b25
olawale-o <omoogunolawale@gmail.com>
2021-08-24 13:30:16 +0100

    Add usecase.dart file

---
+++ lib/core/domain/usecase.dart
@@ -0,0 +1,17 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import '../../core/exceptions/failure.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+class Params extends Equatable {
+  final String email;
+  final String password;
+
+  Params({required this.email, required this.password});
+
+  List<Object?> get props => [email,];
+}
