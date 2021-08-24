f86e40475b39e4c11655ad0f6c31ff12a4e90194
olawale-o <omoogunolawale@gmail.com>
2021-08-24 17:28:26 +0100

    Modify auth_bloc.dart file

--- lib/features/email_auth/presentation/bloc/auth_bloc.dart
+++ lib/features/email_auth/presentation/bloc/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter_auth/features/email_auth/domain/usecases/auth_logout_usecase.dart';
 import 'package:meta/meta.dart';
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
@@ -14,12 +15,15 @@ part 'auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final AuthSignUpUseCase authSignUpUseCase;
   final AuthLoginUseCase authLoginUseCase;
+  final AuthLogoutUseCase authLogoutUseCase;
 
   AuthBloc({
     required AuthSignUpUseCase signUpUseCase,
     required AuthLoginUseCase loginUseCase,
+    required AuthLogoutUseCase logoutUseCase,
 }) : authSignUpUseCase = signUpUseCase,
     authLoginUseCase = loginUseCase,
+    authLogoutUseCase = logoutUseCase,
         super(AuthInitial());
 
   @override
@@ -34,11 +38,19 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       yield AuthLoading();
       final failureOrUser = await authLoginUseCase(Params(email: event.email, password: event.password));
       yield* _eitherFailureOrLoaded(failureOrUser);
+    } else if(event is AuthLogoutEvent) {
+      yield AuthLoading();
+      final failureOrVoid = await authLogoutUseCase(NoParams());
+      yield* _eitherFailureOrVoid(failureOrVoid);
     }
   }
 
   Stream<AuthState> _eitherFailureOrLoaded(Either<Failure, UserModel> failureOrUser) async* {
     yield failureOrUser.fold((e) => AuthError(message: _buildError(e)), (u) => AuthLoaded(userModel: _buildUser(u)));
+  }
+
+  Stream<AuthState> _eitherFailureOrVoid(Either<Failure, void> failureOrVoid) async* {
+    yield failureOrVoid.fold((e) => AuthError(message: _buildError(e)), (u) => AuthInitial());
   }
 
   UserModel _buildUser(UserModel authState) => authState;
