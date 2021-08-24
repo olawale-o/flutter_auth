d336aa36060c5acb4014d2bbd491219e1afd29c8
olawale-o <omoogunolawale@gmail.com>
2021-08-24 13:42:34 +0100

    Register AuthSignUpUseCase and AuthLoginUseCase classes in injection_container.dart file

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/features/email_auth/domain/usecases/auth_login_usecase.dart';
+import 'package:flutter_auth/features/email_auth/domain/usecases/auth_sigup_usecase.dart';
 import 'features/email_auth/data/repository/auth_repository_impl.dart';
 import 'features/email_auth/domain/repository/auth_repository.dart';
 import 'features/email_auth/data/datasource/auth_remote_datasource.dart';
@@ -12,10 +14,13 @@ final s1 = GetIt.instance;
 Future<void> init() async {
   // blocs
   s1.registerFactory(() => NavigationBloc(s1()));
-  
-  // repositiories
 
-    s1.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(authRemoteDataSource: s1()));
+  // repositiories
+  s1.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(authRemoteDataSource: s1()));
+
+  // usecases
+  s1.registerLazySingleton(() => AuthSignUpUseCase(authRepository: s1()));
+  s1.registerLazySingleton(() => AuthLoginUseCase(authRepository: s1()));
 
   // datasources
   s1.registerLazySingleton<AuthRemoteDataSource>(() => AuthRemoteDataSourceImpl(firebaseAuth: s1()));
