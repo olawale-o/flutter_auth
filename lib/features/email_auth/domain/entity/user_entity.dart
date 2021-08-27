c6d4e86bc1a4ed6748cb7b92fe26f58ad5fbd663
olawale-o <omoogunolawale@gmail.com>
2021-08-27 11:35:19 +0100

    Modify user_entity.dart file

--- lib/features/email_auth/domain/entity/user_entity.dart
+++ lib/features/email_auth/domain/entity/user_entity.dart
@@ -2,25 +2,18 @@ import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UserEntity extends Equatable {
-  final AdditionalUserInfo? additionalUserInfo;
-  final AuthCredential? credential;
   final User? user;
 
-  UserEntity({
-    required this.additionalUserInfo,
-    required this.credential,
-    required this.user,});
+  UserEntity({required this.user,});
 
   factory UserEntity.fromJson(dynamic json) {
     return UserEntity(
-      additionalUserInfo: json["additionalUserInfo"],
       user: json["user"],
-      credential: json['credential'],
     );
   }
   @override
   // TODO: implement props
-  List<Object?> get props => [additionalUserInfo, user, credential];
+  List<Object?> get props => [user];
 
 }
 
