d5dacd25c2dacae2588085f8cab449dd7d40beca
olawale-o <omoogunolawale@gmail.com>
2021-08-27 11:34:27 +0100

    Modify user_model.dart file

--- lib/features/email_auth/data/model/user_model.dart
+++ lib/features/email_auth/data/model/user_model.dart
@@ -2,20 +2,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../../domain/entity/user_entity.dart';
 
 class UserModel extends UserEntity {
-  final AdditionalUserInfo? additionalUserInfo;
-  final AuthCredential? credential;
   final User? user;
   UserModel({
-    required this.additionalUserInfo,
-    required this.credential,
     required this.user,
-}) : super(additionalUserInfo: additionalUserInfo, credential: credential, user: user);
+  }) : super(user: user);
 
   factory UserModel.fromJson(dynamic json) {
     return UserModel(
-      additionalUserInfo: json["additionalUserInfo"],
       user: json["user"],
-      credential: json['credential'],
     );
   }
 
