c5c9c6e0648f8798277b11e3c449f7e4184900df
olawale-o <omoogunolawale@gmail.com>
2021-08-27 11:57:34 +0100

    Modify main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'core/presentation/pages/my_app.dart';
 import 'injection_container.dart' as di;
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await di.init();
   runApp(MyApp());
 }
