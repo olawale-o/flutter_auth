ff0343f722ad4e6f69caa28609649fb6997bdf79
olawale-o <omoogunolawale@gmail.com>
2021-09-16 01:53:37 +0100

    Modify validation_mixins.dart file

--- lib/core/mixins/validation_mixins.dart
+++ lib/core/mixins/validation_mixins.dart
@@ -10,7 +10,6 @@ mixin ValidationMixin {
   final validatePassword = StreamTransformer<String, dynamic>.fromHandlers(handleData: (value, sink) {
     if(value.length == 0)  {sink.addError("please enter a password");}
     else if(value.length < 4) {sink.addError("password is too short");}
-    else if(value.length > 6) {sink.addError("password is too long");}
     else {sink.add(true);}
   });
 }
