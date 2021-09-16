891259709708a9f479a5f30a920dd6c55b2e6cb8
olawale-o <omoogunolawale@gmail.com>
2021-09-16 16:12:03 +0100

    Update README.md file

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# flutter_auth
+# Flutter Auth
 
-> Flutter Auth
+> "Flutter Auth" is a project that implement authentication with firebase
+
+## Built with
+
+- Dart
+- Flutter
+- Flutter Bloc
 
 # Credits
-
 - [Icons8](https://icons8.com/)
+- [flutter_bloc](https://pub.dev/packages/flutter_bloc)
---
+++ lib/core/widgets/dismiss_keyboard.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DismissKeyboard extends StatelessWidget {
+  final Widget child;
+  const DismissKeyboard({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onPanDown: (_) {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        onTap: () {
+         FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
+            FocusManager.instance.primaryFocus!.unfocus();
+          }
+        },
+      child: child,
+    );
+  }
+}
